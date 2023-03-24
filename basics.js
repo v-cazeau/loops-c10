6cd5f5bef5056cf64dc214bc952071a6f5b3b8a3
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-03-24 09:57:30 -0400

    did basic loops

--- basics.js
+++ basics.js
@@ -1,6 +1,8 @@
 // Write a loop to print "0. Hello" to "9. Hello" (10 lines)
 
-// write loop here... 
+for(let i = 0; i <10; i++){
+    console.log(`${i}. Hello`)
+}
 
 
 
@@ -8,6 +10,8 @@
 // Write a loop to count down from 10 to 1 (console.log)
 // and then write "Blast Off!"
 
-// write loop here... 
+for(let i = 10 ; i > 0 ; i--){
+    console.log(i)
+}
 
-
+console.log('Blast off!')
