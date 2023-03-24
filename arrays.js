876db72ac1173cd709843f9a28bd932153237327
Todd H. Albert <todd@bocacode.com>
2023-03-24 09:03:05 -0400

    initial setup

---
+++ arrays.js
@@ -0,0 +1,33 @@
+// Given the following array, output each fruit
+
+const fruits = ['apple', 'banana', 'orange', 'kiwi', 'grapes']
+
+// write loop here...
+
+
+// Given the following array, output each student's first name
+
+const students = [
+  {
+    firstName: 'Ilan',
+    lastName: 'Vaks',
+    gender: 'male',
+  },
+  {
+    firstName: 'Kaniel',
+    lastName: 'Tapper',
+    gender: 'male',
+  },
+  {
+    firstName: 'Meghann',
+    lastName: 'Stamper',
+    gender: 'female',
+  },
+  {
+    firstName: 'Meghann',
+    lastName: 'Stamper',
+    gender: 'female',
+  },
+]
+
+// write loop here....
---
+++ basics.js
@@ -0,0 +1,13 @@
+// Write a loop to print "0. Hello" to "9. Hello" (10 lines)
+
+// write loop here... 
+
+
+
+
+// Write a loop to count down from 10 to 1 (console.log)
+// and then write "Blast Off!"
+
+// write loop here... 
+
+
